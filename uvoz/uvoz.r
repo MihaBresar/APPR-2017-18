4af92d9bb1e3c14381b7ad0c76d035e5756813a5
MihaBresar <miha.bresar@gmail.com>
2017-11-30 09:41:16 +0100

    Update uvoz.r

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,4 +1,4 @@
-uvozi.draft <- function() {
+uvozi.draft1 <- function() {
   link <- "https://en.wikipedia.org/wiki/2007_NBA_draft"
   stran <- html_session(link) %>% read_html()
   tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable sortable']") %>%
@@ -8,15 +8,41 @@ uvozi.draft <- function() {
       Encoding(tabela[[i]]) <- "UTF-8"
     }
   }
-  colnames(tabela) <- c("1", "2", "3", "4", "5",
-                        "6", "7")
-  for (col in c("1", "2")) {
+  colnames(tabela) <- c("Krog", "Izbor", "Igralec", "Pozicija", "Državljanstvo",
+                        "Ekipa", "Ekipa pred naborom")
+  for (col in c("Krog", "Izbor")) {
     tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
   }
-  for (col in c("3", "4", "5","6","7")) {
+  for (col in c("Igralec", "Pozicija", "Državljanstvo","Ekipa", "Ekipa pred naborom")) {
     tabela[[col]] <- factor(tabela[[col]])
   }
   
   return(tabela)
 }
+
   
+  
+
+uvozi.draft2 <- function() {
+  link <- "https://en.wikipedia.org/wiki/2008_NBA_draft"
+  stran <- html_session(link) %>% read_html()
+  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable sortable']") %>%
+    .[[1]] %>% html_table(dec = ",")
+  for (i in 1:ncol(tabela)) {
+    if (is.character(tabela[[i]])) {
+      Encoding(tabela[[i]]) <- "UTF-8"
+    }
+  }
+  colnames(tabela) <- c("Krog", "Izbor", "Igralec", "Pozicija", "Državljanstvo",
+                        "Ekipa", "Ekipa pred naborom")
+  for (col in c("Krog", "Izbor")) {
+    tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
+  }
+  for (col in c("Igralec", "Pozicija", "Državljanstvo","Ekipa", "Ekipa pred naborom")) {
+    tabela[[col]] <- factor(tabela[[col]])
+  }
+  
+  return(tabela)
+}
+
+
