8366ab2df09ae227af4734ae8a81072fde53e6bc
MihaBresar <miha.bresar@gmail.com>
2017-12-20 21:06:21 +0100

    Update analiza.r

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,4 @@
 # 4. faza: Analiza podatkov
-
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij = naselja/povrsina) %>%
-  left_join(povprecja, by = "obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
-
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+Moc_izborov <- aggregate(Skupni_podatki$WS, by=list(Skupni_podatki$Pk), FUN=sum)
+colnames(Moc_izborov) <- c("Izbor","Moč")
+Moc_naborov <- aggregate(Skupni_podatki$WS/48, by=list(Skupni_podatki$Leto), FUN=sum)
