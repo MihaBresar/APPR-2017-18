a937bbbeae8ffd93ed3f86c72af429fc4da1da62
MihaBresar <miha.bresar@gmail.com>
2017-12-21 18:28:24 +0100

    Update analiza.r

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,4 +1,13 @@
-# 4. faza: Analiza podatkov
 Moc_izborov <- aggregate(Skupni_podatki$WS, by=list(Skupni_podatki$Pk), FUN=sum)
 colnames(Moc_izborov) <- c("Izbor","Moč")
-Moc_naborov <- aggregate(Skupni_podatki$WS/48, by=list(Skupni_podatki$Leto), FUN=sum)
+Uspešnost_ekip <- aggregate(Skupni_podatki$WS, by=list(Skupni_podatki$Tm), FUN=sum)
+
+ocene_igralcev <-  Skupni_podatki[c(1,2,3,4,18)]
+colnames(ocene_igralcev) <- c("runda","Izbor","Ekipa","Igralec","WS")
+
+ocene_igralcev<- inner_join(ocene_igralcev, Moc_izborov, by = NULL, copy = FALSE)
+
+a <- ocene_igralcev["WS"]/abs(ocene_igralcev["Moč"])
+
+
+
