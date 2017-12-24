cfe7b438de2e78eec25db44319170d67bda49e76
MihaBresar <miha.bresar@gmail.com>
2017-12-24 12:25:01 +0100

    Update vizualizacija.r

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,29 @@
-# 3. faza: Vizualizacija podatkov
+Podatki_csv["WS"] <- abs(Podatki_csv["WS"])
+Moc_izborov <- aggregate(Podatki_csv$WS, by=list(Podatki_csv$Pk), FUN=sum)
+colnames(Moc_izborov) <- c("Izbor","Moč")
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
-                             "OB/OB", encoding = "Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels = levels(obcine$obcina))
-zemljevid <- pretvori.zemljevid(zemljevid)
+ocene_igralcev <-  Skupni_podatki[c(1,2,3,4,18)]
+colnames(ocene_igralcev) <- c("runda","Izbor","Ekipa","Igralec","WS")
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje = sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+ocene_igralcev<- inner_join(ocene_igralcev, Moc_izborov, by = "Izbor", copy = FALSE)
+
+a <- ocene_igralcev["WS"]/abs(ocene_igralcev["Moč"])
+
+ocene_igralcev["uspeh"] <- a
+
+
+
+Uspesnost_ekip1 <- aggregate(Skupni_podatki$WS, by=list(Skupni_podatki$Tm), FUN=sum)
+Uspesnost_ekip2 <- aggregate(ocene_igralcev$uspeh, by=list(ocene_igralcev$Ekipa), FUN=sum)
+Uspesnost_ekip3 <- aggregate(ocene_igralcev$Izbor, by=list(ocene_igralcev$Ekipa), FUN=mean)
+
+colnames(Uspesnost_ekip1) <- c("Ekipa","Uspeh")
+colnames(Uspesnost_ekip2) <- c("Ekipa","Skupna_koristnost")
+colnames(Uspesnost_ekip3) <- c("Ekipa","Povprecen_izbor")
+
+
+Uspesnost_ekip <- merge(Uspesnost_ekip1,Uspesnost_ekip2)
+Uspesnost_ekip <- merge(Uspesnost_ekip,Uspesnost_ekip3)
+rm(Uspesnost_ekip1,Uspesnost_ekip2,Uspesnost_ekip3)
+
+
