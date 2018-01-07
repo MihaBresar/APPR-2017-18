a4f59964130f111c460e9ae13de8d7986ec82eac
MihaBresar <miha.bresar@gmail.com>
2018-01-07 17:06:21 +0100

    Update ui.R

--- shiny/ui.R
+++ shiny/ui.R
@@ -2,16 +2,32 @@ library(shiny)
 
 shinyUI(fluidPage(
   
-  titlePanel("Slovenske občine"),
+  titlePanel("Grafični prikaz"),
   
   tabsetPanel(
-      tabPanel("Velikost družine",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Število naselij",
+    tabPanel("Tabela",
+             hr(),
+             sidebarLayout(
+               
                sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
+                 selectInput("sprem1", label="Izberi spremenljivko",
+                             choices=colnames(Skupni_podatki[c(-4,-14,-16)]), selected='WS')),
+               mainPanel(DT::dataTableOutput("tabele")))
+    ),
+    
+    tabPanel("Graf", 
+
+             sidebarPanel(
+               selectInput("sprem2", label="Izberi spremenljivko",
+                           choices=colnames(nabori[c(-1,-8)]), selected='WS'),
+               checkboxGroupInput('ostalo', label="Izberi države",
+                                               selected=c(1))
+             ),
+             mainPanel(plotOutput("grafi")))
     )
+    
+    
+    
+    
 ))
+    
