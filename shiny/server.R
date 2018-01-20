acb8b2c15d3371d84fec509cd971d1fe2765c474
MihaBresar <miha.bresar@gmail.com>
2018-01-20 18:36:55 +0100

    Add files via upload

--- shiny/server.R
+++ shiny/server.R
@@ -1,16 +1,9 @@
-library(shiny)
-require(stats)
-library(mgcv)
-#source("../vizualizacija/vizualizacija.r", encoding = "UTF-8")
+
+
 
 
 shinyServer(function(input, output) {
-  output$tabele <- DT::renderDataTable({
-    dcast(Skupni_podatki[c('Igralec', 'Pozicija', input$sprem1)], Igralec ~ Pozicija, value.var = input$sprem1)
 
-  })
-  
-  
 output$grafi <- renderPlot({
    tabela <- nabori[c('Nabor', input$sprem2)]
     colnames(tabela) <- c('Nabor', 'sprem')
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,51 +1,43 @@
-library(shiny)
+
 
 shinyUI(fluidPage(
   
   titlePanel("Grafični prikaz"),
   
   tabsetPanel(
-    tabPanel("Tabela",
-             hr(),
-             sidebarLayout(
-               
-               sidebarPanel(
-                 selectInput("sprem1", label="Izberi spremenljivko",
-                             choices=colnames(Skupni_podatki[c(-4,-14,-16)]), selected='WS')),
-               mainPanel(DT::dataTableOutput("tabele")))
-    ),
-    
-   tabPanel("Graf", 
 
+    
+    tabPanel("Graf", 
+             
              sidebarPanel(
-             selectInput("sprem2", label="Izberi spremenljivko",
-                          choices=colnames(nabori[c(-1,-8)]), selected='WS'),
-             checkboxGroupInput('ostalo', label="",
-                                              selected=c(1))
-            ),
+               selectInput("sprem2", label="Izberi spremenljivko",
+                           choices=colnames(nabori[c(-1,-8)]), selected='WS'),
+               checkboxGroupInput('ostalo', label="",
+                                  selected=c(1))
+             ),
              mainPanel(plotOutput("grafi"))),
     
-  
+    
     tabPanel("Primerjava",
-           
-           
-           sidebarPanel(
-             selectInput("spremenljivka1", label="Izberi spremenljivko na x osi",
-                         choices=colnames(Skupni_podatki[c(-1,-2,-3,-4,-14,-15,-16)]), selected='WS'),
-             selectInput("spremenljivka2", label="Izberi spremenljivko na y osi",
-                         choices=colnames(Skupni_podatki[c(-1,-2,-3,-4,-14,-15,-16)]), selected='G'),
-             sliderInput('letnica', label="Izberi nabor",
-                         min=2007, max=2012, step=1, value = 2007),
-             selectInput("priblizek2", label="Izberi regresivno funkcijo:",
-                         choices=c('Izberi funkcijo', 'y ~ x', 'y ~ x + I(x^2)', 'y ~ x + I(x^2) + I(x^3)'), selected='Izberi funkcijo'),
-             sliderInput('skup', label='Izberi število skupin glede na WS',
-                         min=1, max=25, step=1, value = 5)
-
-           ),
-           mainPanel(plotOutput("primerjava")))
+             
+             
+             sidebarPanel(
+               selectInput("spremenljivka1", label="Izberi spremenljivko na x osi",
+                           choices=colnames(Skupni_podatki[c(-1,-2,-3,-4,-14,-15,-16)]), selected='FG%'),
+               selectInput("spremenljivka2", label="Izberi spremenljivko na y osi",
+                           choices=colnames(Skupni_podatki[c(-1,-2,-3,-4,-14,-15,-16)]), selected='Tocke'),
+               sliderInput('letnica', label="Izberi nabor",
+                           min=2007, max=2012, step=1, value = 2007),
+               selectInput("priblizek2", label="Izberi regresivno funkcijo:",
+                           choices=c('Izberi funkcijo', 'y ~ x', 'y ~ x + I(x^2)', 'y ~ x + I(x^2) + I(x^3)'), selected='Izberi funkcijo'),
+               sliderInput('skup', label='Izberi število skupin glede na WS',
+                           min=1, max=25, step=1, value = 5)
+               
+             ),
+             mainPanel(plotOutput("primerjava")))
     
-)
-    
-    
+  )
+  
+  
 ))
     
